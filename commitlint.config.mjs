31046d1a4610dbf5d2aa9a9a62595dd2cd9ce831
NguyenXuanVinh <nongami87@gmail.com>
2026-03-11 18:49:18 +0700

    chore(commitlint): enforce stricter commit message validation
    - improves commit message consistency by tightening validation rules
    for case and line length in both editor and linting configuration.
    - encourages lower-case usage and enforces shorter subject and body
    lines to align with conventional commit standards.

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,4 +1,10 @@
 export default {
   extends: ["@commitlint/config-conventional"],
-  rules: { "body-max-line-length": [2, "always", 120] },
+  rules: {
+    "body-case": [2, "always", "lower-case"],
+    "body-max-line-length": [2, "always", 100],
+    "type-case": [2, "always", "lower-case"],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [2, "always", "lower-case"],
+  },
 };

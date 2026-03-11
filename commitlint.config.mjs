a4badfb24182028a28da3a3204847f3f18b42efe
NguyenXuanVinh <nongami87@gmail.com>
2026-03-11 22:35:07 +0700

    chore(commitlint): remove custom commit message rules
    - removes specific case and line length rules to rely solely on
    the default configuration for commit linting.
    - simplifies maintenance and reduces enforcement overhead.

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,10 +1,3 @@
 export default {
   extends: ["@commitlint/config-conventional"],
-  rules: {
-    "body-case": [2, "always", "lower-case"],
-    "body-max-line-length": [2, "always", 100],
-    "type-case": [2, "always", "lower-case"],
-    "scope-case": [2, "always", "lower-case"],
-    "subject-case": [2, "always", "lower-case"],
-  },
 };

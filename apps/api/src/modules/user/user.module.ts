827e7f00db8018b2cfe4c41f478f0660a75c3838
mtuong1031 <luminhtuongsp@gmail.com>
2026-03-31 22:23:16 +0700

    feat(user): implement user module with service and controller

---
+++ apps/api/src/modules/user/user.module.ts
@@ -0,0 +1,13 @@
+import { Module, forwardRef } from '@nestjs/common';
+
+import { AuthModule } from '../auth/auth.module';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+@Module({
+  imports: [forwardRef(() => AuthModule)],
+  providers: [UserService],
+  controllers: [UserController],
+  exports: [UserService],
+})
+export class UserModule {}

49a550f344da233c81e2a266895c940f72b824bc
NguyenXuanVinh <nongami87@gmail.com>
2026-04-03 14:47:18 +0700

    feat(user): add DTO validation and improve error handling
    - introduces a dedicated data transfer object with validation for user creation.
    
    - enhances error handling in user lookup and creation, throwing specific errors for missing users
    and duplicate emails.
    
    - improves input consistency and aligns error reporting with common API practices.

--- apps/api/src/modules/user/user.service.ts
+++ apps/api/src/modules/user/user.service.ts
@@ -1,28 +1,50 @@
-import type { Prisma, User } from '@repo/db/prisma/client';
-
 import { Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@repo/db/prisma/internal/prismaNamespace';
 
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async findByEmail(email: string): Promise<User | null> {
-    return this.prisma.user.findUnique({
+  async findByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
       where: { email },
     });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
   }
 
-  async findById(id: string): Promise<User | null> {
-    return this.prisma.user.findUnique({
+  async findById(id: string) {
+    const user = await this.prisma.user.findUnique({
       where: { id },
     });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
   }
 
-  async create(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({
-      data,
-    });
+  async create(createUserDto: CreateUserDto) {
+    try {
+      return await this.prisma.user.create({ data: { ...createUserDto } });
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError && error.code === 'P2002') {
+        const target = error.meta?.target as string[] | undefined;
+        const field = target?.[0];
+
+        if (field === 'email') {
+          throw new Error('Email already exist');
+        }
+      }
+      throw error;
+    }
   }
 }

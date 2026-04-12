4de8980b566cefcd197d20dfe9fcace317c6c2c3
mrthinh307 <thinh.ksb.k55@gmail.com>
2026-04-12 22:22:29 +0700

    refactor(ui): edit Checkbox and Input components to use forwardRef for improved ref handling

--- packages/design-system/components/ui/input.tsx
+++ packages/design-system/components/ui/input.tsx
@@ -2,18 +2,22 @@ import { Input as InputPrimitive } from '@base-ui/react/input';
 import { cn } from '@repo/design-system/lib/utils';
 import * as React from 'react';
 
-function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
-  return (
-    <InputPrimitive
-      type={type}
-      data-slot="input"
-      className={cn(
-        'aria-invalid:border-destructive aria-invalid:ring-destructive/20 file:text-foreground border-border focus-visible:border-border bg-background text-foreground disabled:border-disabled disabled:bg-background disabled:text-disabled disabled:placeholder:text-disabled placeholder:text-muted-foreground/70 focus-visible:ring-ring h-11 w-full min-w-0 rounded-md border px-3 py-2.5 text-base shadow-[0_1px_2px_0_rgba(139,92,246,0.10)] transition-all outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:shadow-none focus-visible:ring-2 disabled:cursor-not-allowed aria-invalid:ring-2',
-        className,
-      )}
-      {...props}
-    />
-  );
-}
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <InputPrimitive
+        ref={ref}
+        type={type}
+        data-slot="input"
+        className={cn(
+          'aria-invalid:border-destructive aria-invalid:ring-destructive/20 file:text-foreground border-border focus-visible:border-border bg-background text-foreground disabled:border-disabled disabled:bg-background disabled:text-disabled disabled:placeholder:text-disabled placeholder:text-muted-foreground/70 focus-visible:ring-ring h-11 w-full min-w-0 rounded-md border px-3 py-2.5 text-base shadow-[0_1px_2px_0_rgba(139,92,246,0.10)] transition-all outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:shadow-none focus-visible:ring-2 disabled:cursor-not-allowed aria-invalid:ring-2',
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = 'Input';
 
 export { Input };

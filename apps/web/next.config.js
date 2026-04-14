5727ce3f68170ec9a1da4f145861a27497eec566
mrthinh307 <thinh.ksb.k55@gmail.com>
2026-04-14 23:12:01 +0700

    feat(config): add image remote patterns and update dependencies in package.json

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   output: 'standalone',
   reactCompiler: true,
   typedRoutes: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+      },
+    ],
+  },
   experimental: {
     typedEnv: true,
   },
